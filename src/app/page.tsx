78208f8073760e6cf1c75b38ddc1e0ec83418c94
Éderson Cássio <edersoncassio1983@gmail.com>
2023-12-03 12:50:53 -0300

    Form de login funcionando oO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,41 @@
+'use client'
+
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { SyntheticEvent, useState } from "react"
+
 export default function Home() {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const router = useRouter()
+
+  async function doLogin(event: SyntheticEvent) {
+    event.preventDefault()
+
+    const result = await signIn('credentials',
+      { email, password, redirect: false })
+    
+    if (result?.error) {
+      console.error(result)
+      return
+    }
+
+    router.replace('/admin')
+  }
+
   return (
-    <form>
+    <form onSubmit={doLogin}>
       <p>
-        <input type="text" name="email" placeholder="E-mail" />
+        <input 
+          type="text" name="email" placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </p>
       <p>
-        <input type="password" name="password" placeholder="Senha" />
+        <input 
+          type="password" name="password" placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </p>
       <p className="formButtons">
         <button type="submit">Entrar</button>

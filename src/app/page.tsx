b97dbdca0617e2d8a2216d52619f4960eba6f932
Éderson Cássio <edersoncassio1983@gmail.com>
2023-12-03 12:09:05 -0300

    Form de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 export default function Home() {
   return (
-    <>
-    </>
+    <form>
+      <p>
+        <input type="text" name="email" placeholder="E-mail" />
+      </p>
+      <p>
+        <input type="password" name="password" placeholder="Senha" />
+      </p>
+      <p className="formButtons">
+        <button type="submit">Entrar</button>
+      </p>
+    </form>
   )
 }
